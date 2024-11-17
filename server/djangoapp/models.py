b4e80bba3369e9f42b50d316f02102922b00ac72
Hijas2001 <hijas9089k@gmail.com>
2024-11-17 06:38:12 -0500

    Adding temporary changes to Github

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,18 +1,24 @@
 # Uncomment the following imports before adding the Model code
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
 
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+from django.db import models
 
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)  # Name of the car make
+    description = models.TextField()  # Description of the car make
+    
+    # Any other fields you might want to add
+    # Example: logo_url = models.URLField() for car make's logo
+    
+    def __str__(self):
+        return self.name  # Return the name as the string representation
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many
@@ -23,3 +29,30 @@
 # - Year (IntegerField) with min value 2015 and max value 2023
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-one relationship to CarMake
+    name = models.CharField(max_length=100)  # Name of the car model (e.g., Corolla, Focus)
+    
+    CAR_TYPES = [
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+        # You can add more choices here if needed
+    ]
+    
+    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')  # Car type (Sedan, SUV, Wagon)
+    
+    year = models.IntegerField(
+        default=2023,
+        validators=[
+            MaxValueValidator(2023),
+            MinValueValidator(2015)
+        ]
+    )  # Year of the car, restricted between 2015 and 2023
+    
+    # Any other fields you might want to add
+    # Example: price = models.DecimalField(max_digits=10, decimal_places=2) for price of the car
+    
+    def __str__(self):
+        return f"{self.car_make.name} {self.name}"  # Returns the full name: Make + Model
+
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -7,13 +7,14 @@ from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import logout
 from django.contrib import messages
 from datetime import datetime
+from .models import CarMake, CarModel
 
 from django.http import JsonResponse
 from django.contrib.auth import login, authenticate
 import logging
 import json
 from django.views.decorators.csrf import csrf_exempt
-# from .populate import initiate
+from .populate import initiate
 
 
 # Get an instance of a logger
@@ -21,6 +22,7 @@ logger = logging.getLogger(__name__)
 
 
 # Create your views here.
+
 
 # Create a `login_request` view to handle sign in request
 @csrf_exempt
@@ -98,3 +100,35 @@ def registration(request):
 # Create a `add_review` view to submit a review
 # def add_review(request):
 # ...
+# def get_cars(request):
+#     count = CarMake.objects.filter().count()
+#     print(count)
+#     if(count == 0):
+#         initiate()
+#     car_models = CarModel.objects.select_related('car_make')
+#     cars = []
+#     for car_model in car_models:
+#         cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+#     return JsonResponse({"CarModels":cars})
+
+
+
+from django.http import JsonResponse
+from .models import CarMake, CarModel
+from .populate import initiate
+
+def get_cars(request):
+    # Check if there are any CarModels in the database
+    count = CarModel.objects.count()
+    if count == 0:
+        # If there are no CarModels, call initiate() to populate data
+        initiate()
+
+    # Fetch all car models with their corresponding car makes
+    car_models = CarModel.objects.select_related('car_make')
+
+    cars = []
+    for car_model in car_models:
+        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+
+    return JsonResponse({"CarModels": cars})
